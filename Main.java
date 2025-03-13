23d9659979718434c651dcde23debab6235720a6
s33857 <s33857@pjwstk.edu.pl>
2025-03-13 10:55:49 +0100

    Adding New Java class and new .gitignore file

--- Main.java
+++ Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5, 6};
+        int[] array2 = {2, 3, 4, 5, 6, 7};
+        int[] array3 = {7, 8, 9, 10, 11};
 
+        printArray(filterEvenIndexOddValue(array1));
+        printArray(filterEvenIndexOddValue(array2));
+        printArray(filterEvenIndexOddValue(array3));
+    }
+
+    public static int[] filterEvenIndexOddValue(int[] array) {
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 != 0) {
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            return new int[0];
+        }
+
+        int[] result = new int[count];
+
+        int index = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 != 0) {
+                result[index++] = array[i];
+            }
+        }
+
+        return result;
+    }
+
+    public static void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 }
